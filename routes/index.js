e21f34ea5f4c1f2b20cdb88df65896a6c3c57ebc
vibbyfs <vibbyfs@gmail.com>
2025-07-01 00:19:02 +0700

    add oauth google

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,26 @@ router.get('/', Controller.home)
 router.get('/register', Controller.getRegister)
 router.post('/register', Controller.postRegister)
 
+router.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+router.get('/auth/google/dashboard', passport.authenticate('google', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/login',
+}))
+
 router.get('/login', Controller.getLogin)
 router.post('/login', passport.authenticate('local', {
     successRedirect: '/dashboard',
     failureRedirect: '/login',
 }))
+
+router.get('/logout', (req, res)=> {
+    req.logout((err) => {
+        if (err) res.send(err);
+        res.redirect('/')
+    })
+})
 
 router.get('/dashboard', Controller.getDashboard)
 
